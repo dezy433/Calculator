66df580e555d4e5c528da914134364e5f2e78678
dez433 <derenertim@hotmail.com>
2022-03-08 16:18:45 +0000

    added calculator

---
+++ main.py
@@ -0,0 +1,51 @@
+
+"""
+def equaltionals(num1,num2):
+        While True:
+        maths_var = input(f"Please Enter the equation{equaltionals}")
+        if maths_var()  "+":
+            print(num1+num2)
+        elif
+
+    return math_var
+"""
+def add( num1,num2):
+    return num1 + num2
+
+def subtract( num1, num2):
+    return num1 - num2
+
+def divide( num1, num2):
+    return num1/num2
+
+def multiply(num1, num2):
+    return num1*num2
+choice = int(input("Enter your mathematic equation "))
+math_count = int(maths_equations)
+for math_equations in range(maths_equations):
+   """"
+    additional =
+    subtractions =
+    division =
+"""
+
+
+
+
+    num1 = int(input("Please add the first number:"))
+   num2 = int(input("Please add the second number:"))
+
+if choice == int(1):
+    print(num1, "+",num2, "=", add(num1 + num2))
+
+elif choice == int(2):
+    print(num1, "-",num2,"=", subtract(num1,num2))
+
+elif choice == int(3):
+    print(num1,"/",num2,"=",divide(num1,num2))
+
+elif choice == int(4):
+    print(num1,"x",num2,"=",multiply(num1,num2))
+else:
+    print("DONE")
+
