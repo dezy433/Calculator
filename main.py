faabaac89e1a4ed27ed706988fd7aad98acd4d4f
dez433 <derenertim@hotmail.com>
2022-03-08 21:26:37 +0000

    Added new version of calculator but still, only 2 numbers

--- main.py
+++ main.py
@@ -1,51 +1,50 @@
 
-"""
-def equaltionals(num1,num2):
-        While True:
-        maths_var = input(f"Please Enter the equation{equaltionals}")
-        if maths_var()  "+":
-            print(num1+num2)
-        elif
-
-    return math_var
-"""
-def add( num1,num2):
-    return num1 + num2
-
-def subtract( num1, num2):
-    return num1 - num2
-
-def divide( num1, num2):
-    return num1/num2
-
-def multiply(num1, num2):
-    return num1*num2
-choice = int(input("Enter your mathematic equation "))
-math_count = int(maths_equations)
-for math_equations in range(maths_equations):
-   """"
-    additional =
-    subtractions =
-    division =
-"""
+def add(x, y):
+ while True:
+    if number.isdigit():
+     else number x + y :
+        return x + y
 
 
 
 
-    num1 = int(input("Please add the first number:"))
-   num2 = int(input("Please add the second number:"))
+def subtract(x, y):
+    return x - y
 
-if choice == int(1):
-    print(num1, "+",num2, "=", add(num1 + num2))
 
-elif choice == int(2):
-    print(num1, "-",num2,"=", subtract(num1,num2))
+def multiply(x, y):
+    return x * y
 
-elif choice == int(3):
-    print(num1,"/",num2,"=",divide(num1,num2))
 
-elif choice == int(4):
-    print(num1,"x",num2,"=",multiply(num1,num2))
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice = input("Enter choice(1/2/3/4):")
+number = 0
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+while number !="":
+
+    number= input("Enter number or hit eneter to quit")
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
 else:
-    print("DONE")
-
+    "Done"
